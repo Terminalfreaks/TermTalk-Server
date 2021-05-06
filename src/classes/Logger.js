3e6054c9ff80eac9b85ba37932f459ec08a6ff03
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2021-05-05 22:09:54 -0400

    fix config accessors, change logger to use '.'

--- src/classes/Logger.js
+++ src/classes/Logger.js
@@ -12,12 +12,12 @@ class Logger {
         this.#Title = title.charAt(0).toUpperCase() + title.substring(1);
     };
     error(error) {
-        if (error.stack && error.message && typeof error.stack === 'string' && typeof e.message === 'string') console.log('[' + new Date().toLocaleDateString() + ' @ ' + new Date().toLocaleTimeString() + ' | Error | ' + this.#Title + ']: ' + error.message);
+        if (error.stack && error.message && typeof error.stack === 'string' && typeof e.message === 'string') console.log('[' + new Date().toLocaleDateString() + ' @ ' + new Date().toLocaleTimeString() + ' | Error | ' + this.#Title + ']: ' + error.message + '.');
         else if (typeof error == 'string' || Config.Logger.logUnknownTypes) console.log('[' + new Date().toLocaleDateString() + ' @ ' + new Date().toLocaleTimeString() + ' | ' + this.#Title + ']: ' + error + '.');
         else return; // If it isn't a basic string (assumed manually passed in), or a standard NodeJS Error Object, then don't log it (unless enabled)
     }
     write(message) {
-        console.log('[' + new Date().toLocaleDateString() + ' @ ' + new Date().toLocaleTimeString() + ' | ' + this.#Title + ']: ' + message);
+        console.log('[' + new Date().toLocaleDateString() + ' @ ' + new Date().toLocaleTimeString() + ' | ' + this.#Title + ']: ' + message + '.');
     }
     exit(error) {
         this.error(error);
--- src/classes/MySQL.js
+++ src/classes/MySQL.js
@@ -24,20 +24,20 @@ class MySQL {
    #createPool() {
       try {
          this.#checkConfig();
-         this.#Pool = Driver.createPool(this.#Config.MySQL);
+         this.#Pool = Driver.createPool(this.#Config);
       } catch (error) {
          this.Log.exit(error);
       } finally {
-         this.Log.write('Created new MySQL Pool @ ' + this.#Config.MySQL.host);
+         this.Log.write('Created new MySQL Pool @ ' + this.#Config.host);
          this.Connection = this.#Pool.getConnection();
       }
    }
    #checkConfig() {
       // Does the config have the defaults still?
       // Does the config have missing required components?
-      if (this.#Config.MySQL.user == 'root') this.Log.write('User is set to root, this is not recommended.');
-      if (this.#Config.MySQL.connectionLimit < 20) this.Log.write('Connection limit is below 20, this may cause issues.');
-      if (!this.#Config.MySQL.host || !this.#Config.MySQL.user || !this.#Config.MySQL.key) this.Log.exit('Missing some required configuration details.');
+      if (this.#Config.user == 'root') this.Log.write('User is set to root, this is not recommended');
+      if (this.#Config.connectionLimit < 20) this.Log.write('Connection limit is below 20, this may cause issues');
+      if (!this.#Config.host || !this.#Config.user || !this.#Config.key) this.Log.exit('Missing some required configuration details');
    }
 }
 
