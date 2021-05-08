3ead0e77f2e1240cf7e8c023fb8b062d7ac05ddf
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2021-05-08 16:48:36 -0400

    bare minimum for testing

--- src/database/SQLite.js
+++ src/database/SQLite.js
@@ -6,11 +6,16 @@
 
 // import Driver from 'better-sqlite3';
 import Database from './Database.js';
-
+import SQLite3 from 'better-sqlite3';
+import dirname from 'es-dirname';
+import Path from 'path';
 /** Main SQLite Class */
 class SQLite extends Database {
+    static Connection;
     constructor() {
         super('SQLite');
+        this.Connection = new SQLite3(Path.join(decodeURI(dirname()), '/termtalk.db'), { fileMustExist: true });
+        super.check(this.Connection);
     }
 }
 export default SQLite;
