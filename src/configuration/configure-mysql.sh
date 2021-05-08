71d39765940cd8ee0c30e4ebd3cfda04c78a9d24
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2021-05-08 16:47:47 -0400

    update configuration

--- src/configuration/configure-mysql.sh
+++ src/configuration/configure-mysql.sh
@@ -22,10 +22,82 @@ echo "################################################################"
 # Do the actual generation
 echo "One moment..."
 sudo mysql -u root << COMMANDS
-CREATE DATABASE $MAINDB;
-CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASS}';
-GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';
-FLUSH PRIVILEGES;
+create database if not exists $MAINDB;
+create table if not exists banned
+(
+	id bigint not null,
+	timestamp text default current_timestamp() not null,
+	reason text default 'Unspecified' null,
+	constraint banned_id_uindex
+		unique (id)
+);
+
+create table if not exists messages
+(
+	id bigint auto_increment,
+	messageHash text not null,
+	timestamp text default current_timestamp() not null,
+	roomID bigint not null,
+	user varchar(64) not null,
+	constraint messages_id_uindex
+		unique (id)
+);
+
+create index if not exists messages_roomID_index
+	on messages (roomID);
+
+create table if not exists rooms
+(
+	name varchar(64) not null,
+	passkey text not null,
+	topic text default 'Unset' not null,
+	created text default current_timestamp() not null,
+	id bigint(64) not null,
+	constraint rooms_id_uindex
+		unique (id)
+);
+
+create table if not exists sessions
+(
+	id bigint not null,
+	connectedAt text default current_timestamp() not null,
+	failedAttempts int default 0 not null,
+	resync text not null,
+	constraint sessions_id_uindex
+		unique (id)
+);
+
+create table if not exists userRooms
+(
+	roomID bigint not null,
+	userID bigint not null,
+	timestamp text default current_timestamp() not null
+);
+
+create index if not exists userRooms_roomID_index
+	on userRooms (roomID);
+
+create index if not exists userRooms_userID_index
+	on userRooms (userID);
+
+create table if not exists users
+(
+	id bigint not null,
+	username varchar(64) not null,
+	passwordHash text not null,
+	tag varchar(10) not null,
+	uid varchar(64) not null,
+	constraint users_id_uindex
+		unique (id),
+	constraint users_uid_uindex
+		unique (uid)
+);
+
+
+
+create user ${MAINDB}@localhost identified by '${PASS}';
+grant all privileges on ${MAINDB}.* to '${MAINDB}'@'localhost';
+flush privileges;
 COMMANDS
 # Print out when done
 echo "################################################################"
